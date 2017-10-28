3c8d07ffa31835eb5bb6cf3ca101b0ae3a6d554c
Paul Jickling <pjickling@gmail.com>
2017-10-28 09:52:38 -0700

    Arrays created with scrape tool

--- scrape.js
+++ scrape.js
@@ -1,14 +1,30 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('http://www.dota2.com/procircuit');
 
-  // #playersBody .playersElement #player0, player1, player2, etc. .playerNameColumn .pointsColumn
+// to see how the NodeList object works see https://developer.mozilla.org/en-US/docs/Web/API/NodeList
 
-  let player = await page.evaluate(() => document.querySelector('#player0 .playerNameColumn').textContent);
-  console.log(player);
+  const nameResult = await page.evaluate(() => {
+   let nameArr = [];
+   let playerColumn = document.querySelectorAll('span.columnContent.playerNameColumn');
+   for (let i=0; i < playerColumn.length; i++) {
+     nameArr.push(playerColumn.item(i).textContent);
+   }
+   return nameArr;
+  });
+
+  const pointsResult = await page.evaluate(() => {
+    let pointsArr = [];
+    let pointsColumn = document.querySelectorAll('span.columnContent.pointsColumn');
+    for (let i=0; i < pointsColumn.length; i++) {
+      pointsArr.push(pointsColumn.item(i).textContent);
+    }
+    return pointsArr;
+  });
 
   await browser.close();
 })();
