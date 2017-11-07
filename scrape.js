59b465f0bf7ab05065dc9da9febed9dc991e7300
Paul Jickling <pjickling@gmail.com>
2017-11-06 20:01:58 -0800

    Scrape.js writes JSON file

--- scrape.js
+++ scrape.js
@@ -1,3 +1,5 @@
+module.exports = function() {
+
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
@@ -8,9 +10,18 @@ const fs = require('fs');
 
 // to see how the NodeList object works see https://developer.mozilla.org/en-US/docs/Web/API/NodeList
 
+  class Player {
+    constructor(name, points) {
+      this.name = name;
+      this.points = points;
+    }
+  }
+
+  let players = [];
+
   const nameResult = await page.evaluate(() => {
    let nameArr = [];
-   let playerColumn = document.querySelectorAll('span.columnContent.playerNameColumn');
+   let playerColumn = document.querySelectorAll('.playersElement > span.columnContent.playerNameColumn');
    for (let i=0; i < playerColumn.length; i++) {
      nameArr.push(playerColumn.item(i).textContent);
    }
@@ -19,12 +30,22 @@ const fs = require('fs');
 
   const pointsResult = await page.evaluate(() => {
     let pointsArr = [];
-    let pointsColumn = document.querySelectorAll('span.columnContent.pointsColumn');
+    let pointsColumn = document.querySelectorAll('.playersElement > span.columnContent.pointsColumn');
     for (let i=0; i < pointsColumn.length; i++) {
       pointsArr.push(pointsColumn.item(i).textContent);
     }
     return pointsArr;
   });
 
+  for (let i=0; i < nameResult.length; i++) {
+    let p = new Player(nameResult[i], pointsResult[i]);
+    players.push(p);
+  }
+
+fs.writeFile('./public/points.json', JSON.stringify(players), (err) => {
+  if (err) throw err;
+});
+
   await browser.close();
 })();
+}
