94c9ccf0f64abe74d872732e548a0e4ce4d0ce27
Paul Jickling <pjickling@gmail.com>
2017-12-14 20:10:48 -0800

    Fixed sorting script

--- public/sort.js
+++ public/sort.js
@@ -10,26 +10,17 @@ let rank = document.querySelector('li#rank');
 
 rank.addEventListener('click', function() {
   let rankList = [];
-  let rawRank = [];
   let rankSelector = document.querySelectorAll('span.rank');
 
   for (let i=0; i < rankSelector.length; i++) {
     let n = i+1;
     let selection = document.querySelector(`.container > a:nth-child(${n})`); //selects appropriate card
     let sorter = new Sorter(selection, parseFloat(rankSelector.item(i).textContent), null);
-    rawRank.push(parseFloat(rankSelector.item(i).textContent));
     rankList.push(sorter);
   }
-  console.log(rawRank);
-  rawRank.sort((a, b) => a - b);
-  console.log(rawRank);
-  // now i need to think about how to have the position of the arrays so that they are equivalent.
-  for (let r=0; r < rawRank.length; r++) {
-    console.log(rawRank[r], rankList[r]);
-    if (rawRank[r] !== rankList[r].rank) {
-
-    }
-  }
+  rankList.sort(function(a, b) {
+    return (a.rank) - (b.rank);
+  });
 
   let parentDiv = rankList[0].selection.parentNode;
 
@@ -47,18 +38,18 @@ let points = document.querySelector('li#points');
 
 points.addEventListener('click', function() {
   let pointsList = [];
-  let rawPoints = [];
   let pointsSelector = document.querySelectorAll('span.pointsRank');
 
   for (let i=0; i < pointsSelector.length; i++) {
     let n = i+1;
     let selection = document.querySelector(`.container > a:nth-child(${n})`);
     let sorter = new Sorter(selection, null, parseFloat(pointsSelector.item(i).textContent));
-    rawPoints.push(parseFloat(rankSelector.item(i).textContent));
     pointsList.push(sorter);
   }
 
-  pointsList.sort((a, b) => a - b);
+  pointsList.sort(function(a, b) {
+    return (a.points) - (b.points);
+  });
   let parentDiv = pointsList[0].selection.parentNode;
 
   for (let x = pointsList.length; x > 1; x--) {
