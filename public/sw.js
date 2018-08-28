354720f35acc15de6585d903e9a222ed8cfcc548
Paul Jickling <pjickling@gmail.com>
2018-08-28 14:29:16 -0700

    Added service worker script

---
+++ public/sw.js
@@ -0,0 +1,42 @@
+let CACHE_NAME = 'dreamstream-cache-v1';
+let urlsToCache = [
+  '/',
+  '/style.css',
+  '/loadjson.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  let cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
